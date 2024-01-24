39f80f61a46882a53921a873f7a2b8fc9bf2ea10
Eda Kalaycioglu <edakalaycioglu@gmail.com>
2024-01-24 03:05:36 +0300

    project is completed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
+import model.Player;
+import model.Point;
+import model.Weapon;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Point first = new Point(6, 5);
+
+        Point second = new Point(3, 1);
+
+        System.out.println("distance(0,0)= " + first.distance());
+
+        System.out.println("distance(second)= " + first.distance(second));
+
+        System.out.println("distance(2,2)= " + first.distance(2, 2));
+
+        Point point = new Point(0,0);
+
+        System.out.println("distance()= " + point.distance());
+
+        System.out.println("*************");
+
+        Player player = new Player("EDA", 100, Weapon.KNIFE);
+        System.out.println("healthRemaining: " + player.healthRemaining());
+        player.loseHealth(30);
+        System.out.println("healthRemaining: " + player.healthRemaining());
+        player.restoreHealth(20);
+        System.out.println("healthRemaining: " + player.healthRemaining());
     }
 }
---
+++ src/model/Weapon.java
@@ -0,0 +1,7 @@
+package model;
+
+public enum Weapon {
+        MACHINEGUN,
+        KNIFE,
+        CANNON,
+}
